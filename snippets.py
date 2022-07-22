e0621f15f0453734f9e0473e966526d82261a966
Nils Blume <mail@nilsblume.de>
2022-07-22 16:22:32 +0200

    update snippets

--- snippets.py
+++ snippets.py
@@ -1,11 +1,17 @@
+from urllib.parse import urlparse
+from pymongo import MongoClient
+client = MongoClient("mongodb://phoenixinserter:phoenix@localhost:27017/phoenixarchive")
+db = client.phoenixarchive
 # Snippets
 
-coll = db['devilsfilm']
+coll = db['kink']
 
 while True:
-   try:
-     doc = coll.find_one({"id_2": {"$exists": False}})
-     doc['id_2'] = urlparse(doc['url']).path.rsplit('/', 2)[-2]
-     coll.find_one_and_update({"_id": doc['_id']}, {"$set": doc})
-   except:
-     break
+  try:
+    doc = coll.find_one({"id": {"$exists": False}})
+    print(doc['_id'])
+    doc['id'] = urlparse(doc['url']).path.rsplit('/', 1)[-1]
+    #doc['id_2'] = urlparse(doc['url']).path.rsplit('/', 2)[-2]
+    coll.find_one_and_update({"_id": doc['_id']}, {"$set": doc})
+  except:
+    break
