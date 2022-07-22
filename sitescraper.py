50d72a31bb148ca2f264f18a2c3f5f1c5f396990
Nils Blume <mail@nilsblume.de>
2022-07-22 16:29:41 +0200

    fixed imports

--- sitescraper.py
+++ sitescraper.py
@@ -1,3 +1,6 @@
+import os
+from urllib import request
+import dns
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
