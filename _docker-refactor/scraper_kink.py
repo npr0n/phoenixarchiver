591e77c94e70fef22f36d496b64b4e47b3e1a668
npr0n <npronporn@gmail.com>
2023-02-02 10:14:54 +0100

    removed additional space

--- _docker-refactor/scraper_kink.py
+++ _docker-refactor/scraper_kink.py
@@ -14,14 +14,26 @@ sites = [
   "kink"
 ]
 
-def kink_scraper(driver, doc, getmaxtries: int = 3, findmaxtries: int = 3):
+def kink_is404(driver):
+  # 404
+  try:
+    if driver.find_element(By.CLASS_NAME, "four-oh-four"):
+      print("got 404 page")
+      return 404
+  except:
+    return 0
+
+def kink_scraper(driver, doc, getmaxtries: int = 1, findmaxtries: int = 1, verbose: bool = False):
   # get page
   for attempt in range(getmaxtries):
     try:
       actors = []
       categories = []
       driver.get(doc['url'])
+      if verbose:
+        print(doc['url'])
     except TimeoutException:
+      print("page timed out")
       sleep(3)
       continue
     else:
@@ -33,7 +45,11 @@ def kink_scraper(driver, doc, getmaxtries: int = 3, findmaxtries: int = 3):
   # title
   for attempt in range(findmaxtries):
     try:
+      if verbose:
+        print("title", attempt)
       doc['title'] = driver.find_element(By.XPATH, "//head/title").get_attribute("innerText")
+      if "404" in doc['title']:
+        return doc
     except NoSuchElementException:
       continue
     else:
@@ -42,6 +58,8 @@ def kink_scraper(driver, doc, getmaxtries: int = 3, findmaxtries: int = 3):
   # description
   for attempt in range(findmaxtries):
     try:
+      if verbose:
+        print("description", attempt)
       doc['description'] = driver.find_element(By.XPATH, "//span[@class= 'description-text']").get_attribute("innerText")
     except NoSuchElementException:
       continue
@@ -51,6 +69,8 @@ def kink_scraper(driver, doc, getmaxtries: int = 3, findmaxtries: int = 3):
   # date (site format)
   for attempt in range(findmaxtries):
     try:
+      if verbose:
+        print("date site format", attempt)
       doc['datesite'] = driver.find_element(By.XPATH, "//span[@class= 'shoot-date']").get_attribute("innerText")
     except NoSuchElementException:
       continue
@@ -66,9 +86,13 @@ def kink_scraper(driver, doc, getmaxtries: int = 3, findmaxtries: int = 3):
   # poster url
   for attempt in range(findmaxtries):
     try:
+      if verbose:
+        print("posterurl", attempt)
       doc['posterurl'] = driver.find_element(By.XPATH, "//video[@class= 'vjs-tech']").get_attribute("poster")
     except NoSuchElementException:
       try:
+        if verbose:
+          print("posterurl2", attempt)
         doc['posterurl'] = driver.find_element(By.XPATH, "//div[@class= 'player']/img").get_attribute("src")
       except:
         continue
@@ -78,6 +102,8 @@ def kink_scraper(driver, doc, getmaxtries: int = 3, findmaxtries: int = 3):
   # channel
   for attempt in range(findmaxtries):
     try:
+      if verbose:
+        print("channel", attempt)
       doc['channel'] = driver.find_element(By.XPATH, "//div[contains(@class, 'shoot-logo')]/a").get_attribute("href").rsplit('/', 1)[-1]
     except NoSuchElementException:
       continue
@@ -87,6 +113,8 @@ def kink_scraper(driver, doc, getmaxtries: int = 3, findmaxtries: int = 3):
   # director
   for attempt in range(findmaxtries):
     try:
+      if verbose:
+        print("director", attempt)
       doc['director'] = driver.find_element(By.XPATH, "//span[@class= 'director-name']").get_attribute("innerText")
     except NoSuchElementException:
       continue
@@ -96,6 +124,8 @@ def kink_scraper(driver, doc, getmaxtries: int = 3, findmaxtries: int = 3):
   # rating
   for attempt in range(findmaxtries):
     try:
+      if verbose:
+        print("rating", attempt)
       doc['rating'] = driver.find_element(By.XPATH, "//div[contains(@class, 'shoot-info')]//span[contains(@class, 'thumb-up-percentage')]").get_attribute("innerText")
     except NoSuchElementException:
       continue
@@ -105,6 +135,8 @@ def kink_scraper(driver, doc, getmaxtries: int = 3, findmaxtries: int = 3):
   # actors
   for attempt in range(findmaxtries):
     try:
+      if verbose:
+        print("actors", attempt)
       for actor in (driver.find_elements(By.XPATH, "//p[@class= 'starring']/span[contains(@class, 'names')]/a")):
         actors.append(actor.get_attribute("innerText").split(',', 1)[0])
       doc['actors'] = actors
@@ -116,6 +148,8 @@ def kink_scraper(driver, doc, getmaxtries: int = 3, findmaxtries: int = 3):
   # categories
   for attempt in range(findmaxtries):
     try:
+      if verbose:
+        print("categories", attempt)
       for category in (driver.find_elements(By.XPATH, "//a[@class= 'tag']")):
         categories.append(category.get_attribute("innerText"))
       doc['categories'] = categories
@@ -124,12 +158,17 @@ def kink_scraper(driver, doc, getmaxtries: int = 3, findmaxtries: int = 3):
     else:
       break
   
+  if verbose:
+    print("parsing completed")
+
   return doc
 
 
-def kink_scraper_main(mongoUri = MONGODB_URI, mongoDB = MONGODB_DATABASE, sites = sites, useragent = SELENIUM_USERAGENT, command_executor = SELENIUM_URI, headless = SELENIUM_HEADLESS, driver_iwait: int = 30):
+def kink_scraper_main(mongoUri = MONGODB_URI, mongoDB = MONGODB_DATABASE, sites = sites, useragent = SELENIUM_USERAGENT, command_executor = SELENIUM_URI, headless = SELENIUM_HEADLESS, driver_iwait: int = 10, verbose: bool = False):
   # mongodb connection
   try:
+    if verbose:
+      print("setting up db connection")
     db = init_db(mongoUri, mongoDB)
   except:
     print("error setting up db connection")
@@ -137,6 +176,8 @@ def kink_scraper_main(mongoUri = MONGODB_URI, mongoDB = MONGODB_DATABASE, sites 
   
   # webdriver
   try:
+    if verbose:
+      print("starting webdriver")
     driver = init_driver(command_executor = command_executor, useragent = useragent, driver_iwait = driver_iwait, headless =  headless)
   except:
     print("error setting up webdriver")
@@ -144,6 +185,8 @@ def kink_scraper_main(mongoUri = MONGODB_URI, mongoDB = MONGODB_DATABASE, sites 
   
   # cookie closer
   try:
+    if verbose:
+      print("closing cookie warning")
     cookie_warn_close(driver)
   except:
     print("cookie warning closer failed")
@@ -160,20 +203,32 @@ def kink_scraper_main(mongoUri = MONGODB_URI, mongoDB = MONGODB_DATABASE, sites 
       # find database entry without title
       try:
         doc = find_one_no_title(collection)
+        if verbose:
+          if doc == None:
+            print("did not find entry without title")
+            break
+          else:
+            print("found entry without title")
+            print(doc)
       except:
         break
       
       # scrape page and update doc
       try:
-        doc = kink_scraper(driver = driver, doc = doc)
+        doc = kink_scraper(driver = driver, doc = doc, verbose = verbose)
       except:
-        print("error parsing page")
+        print("error parsing page", driver.current_url)
         break
 
       # update database entry
       try:
-        upsert(collection = collection, object = doc, key = "_id")
+        upsert(collection = collection, doc = doc, key = "_id")
+        if verbose:
+          print("updated db entry")
       except:
         print("error updating database", collection)
-
+  if verbose:
+    print("sleeping for 30s")
+    sleep(30)
+  
   driver.quit()
