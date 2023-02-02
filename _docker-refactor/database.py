96d65ab98b0894e73a1f5b6ed560d0c4e481b70d
npr0n <npronporn@gmail.com>
2023-02-02 10:15:04 +0100

    debugging features

--- _docker-refactor/database.py
+++ _docker-refactor/database.py
@@ -13,12 +13,18 @@ def init_default_db(uri = MONGODB_URI, database = MONGODB_DATABASE):
   db = init_db(uri = uri, database = database)
   return db
 
-def upsert(collection, object, key: str):
+def upsert(collection, doc, key: str):
   try:
-    collection.update_one({f"{key}": object[key]}, {"$set": object}, upsert=True)
+    collection.update_one({f"{key}": doc[key]}, {"$set": doc}, upsert=True)
   except pymongo.errors.DuplicateKeyError:
     # this is most likely due to a race condition because of upsert and multiple keys
     return 0
+
+def find_one_and_delete(collection, doc, key: str):
+  try:
+    collection.find_one_and_delete({f"{key}": doc[key]})
+  except:
+    print("could not delete doc from db:", doc)
 
 def find_one_no_title(collection):
   try:
