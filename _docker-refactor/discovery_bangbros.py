ce494d84433d2a91e140a5f11d1bf4f61b525168
npr0n <npronporn@gmail.com>
2023-02-09 21:30:28 +0000

    init

---
+++ _docker-refactor/discovery_bangbros.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+from variables import *
+from webdriver import *
+from database import *
+from time import sleep
+
+### SITE CONFIG ###
+sites = [
+
+{
+  "baseUrl": "https://bangbros.com/videos/1",
+  "resultSearchPattern": "//a[contains(@class, 'thmb_lnk')]",
+  "nextPageSearchPattern": "pagination_btn_next",
+  "method": "ID",
+  "collection": "bangbros",
+  "channelSearchPattern": "..//a[contains(@class, 'thmb_mr_lnk')]",
+  "dateSearchPattern": "..//span[contains(@class, 'thmb_mr_2')]/span[contains(@class, 'faTxt')]"
+}
+]
+
+
+def bangbros_discovery_loop(db, driver, site: dict, maxPage: int = 10):
+  
+  try:
+    discover_site(db = db, driver = driver, site = site, maxPage = maxPage, navsleep = 1, scrollOffset = 30)
+  except:
+    print("an error occurred")
+  
+
+def bangbros_discovery_main(mongoUri = MONGODB_URI, mongoDB = MONGODB_DATABASE, sites = sites, useragent = SELENIUM_USERAGENT, command_executor = SELENIUM_URI, headless = SELENIUM_HEADLESS, maxPage = DISCOVERY_MAXPAGES, driver_iwait: int = 30, initPage: int = 1):
+  # mongodb connection
+  try:
+    db = init_db(mongoUri, mongoDB)
+  except:
+    print("error setting up db connection")
+    return 1
+  
+  # webdriver
+  try:
+    driver = init_driver(command_executor = command_executor, useragent = useragent, driver_iwait = driver_iwait, headless = headless)
+  except:
+    print("error setting up webdriver")
+    return 1
+
+  for site in sites:
+    try:
+      bangbros_discovery_loop(db = db, driver = driver, site = site, maxPage = maxPage)
+    except:
+      continue
+  
+  driver.quit()
