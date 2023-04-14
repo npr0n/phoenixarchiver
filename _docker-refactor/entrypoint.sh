4f0415b1c56d1bde496786ce286898cdfd37388d
npr0n <npronporn@gmail.com>
2023-04-14 13:17:16 +0000

    added verbosity.

--- _docker-refactor/entrypoint.sh
+++ _docker-refactor/entrypoint.sh
@@ -2,4 +2,9 @@
 printenv > /etc/environment
 echo PYTHONPATH=/usr/local/lib/python >> /etc/environment
 
+echo "set up environment for cron."
+echo "# crontab -l:"
+crontab -l
+
+echo "starting cron."
 cron -f
--- _docker-refactor/variables.py
+++ _docker-refactor/variables.py
@@ -4,7 +4,7 @@ import os
 
 # GET ENVIRONMENT VARIABLES
 SELENIUM_USERAGENT = os.getenv('SELENIUM_USERAGENT', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36')
-SELENIUM_HEADLESS = os.getenv('SELENIUM_HEADLESS', 'False').lower() in ('true', 't', '1', '0')
+SELENIUM_HEADLESS = os.getenv('SELENIUM_HEADLESS', 'True').lower() in ('true', 't', '1', '0')
 SELENIUM_URI = os.getenv('SELENIUM_URI', 'http://127.0.0.1:4444/wd/hub')
 
 MONGODB_URI = os.getenv('MONGODB_URI', "mongodb://phoenixinserter:phoenix@localhost:27107/phoenixarchive")
@@ -12,4 +12,4 @@ MONGODB_DATABASE = os.getenv('MONGODB_DATABASE', "phoenixarchive")
 
 DISCOVERY_MAXPAGES = int(os.getenv('DISCOVERY_MAXPAGES', "10"))
 
-VERBOSE = os.getenv('VERBOSE', 'True')
+VERBOSE = os.getenv('VERBOSE', 'True').lower() in ('true', 't', '1', '0')
