5e9ac38b9e9b03430f238920217990d5e6c357a8
npr0n <npronporn@gmail.com>
2023-04-17 11:11:49 +0000

    removed unused import

--- _docker-refactor/wdriver.py
+++ _docker-refactor/wdriver.py
@@ -6,7 +6,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import ElementClickInterceptedException
 from urllib.parse import urlparse
 from time import sleep
 
