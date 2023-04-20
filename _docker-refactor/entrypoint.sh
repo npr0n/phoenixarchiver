195b419a74d087e944719c880ac59fac7b06310d
npr0n <npronporn@gmail.com>
2023-04-20 10:14:40 +0000

    updated env handler

--- _docker-refactor/entrypoint.sh
+++ _docker-refactor/entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 echo "setting up environment variables for cron"
 printenv > /etc/environment
-if [ -f /app/.env ]
-then
-  cat /app/.env >> /etc/environment
-fi
-cat /etc/environment
-
 
 echo "set up environment for cron."
 echo "# crontab -l:"
